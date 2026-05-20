4b41a918a4c40dc9ff63056dfdd27a20ff37fc72
Victor Toupitzen Specian <victor.specian@kobel.com.br>
2026-05-20 13:44:09 -0300

    feat: Add environment configuration module
    Integrates the `@nestjs/config` package to provide a structured way to load and access application configuration from environment variables.
    
    This sets up an `EnvConfigModule` and `EnvConfigService` to centralize access to settings like `PORT` and `NODE_ENV`, improving maintainability and testability.

---
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { EnvConfig } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class EnvConfigService implements EnvConfig {
+
+    constructor(private configService: ConfigService) {}
+
+    getAppPort(): number {
+        return Number(this.configService.get<Number>('PORT'));
+    }
+
+    getNodeEnv(): string {
+        return this.configService.get<string>('NODE_ENV') as string;
+    }
+}
