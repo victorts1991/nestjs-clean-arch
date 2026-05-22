febee537f849ddaed64bdeba0c3bb6b6592346db
Victor Toupitzen Specian <victor.specian@kobel.com.br>
2026-05-22 16:28:14 -0300

    feat: Introduce base Entity class and user data builder
    Extracts common entity properties like ID management and basic serialization into an abstract `Entity` class, promoting reusability and consistency across domain models.
    
    Refactors user entity tests to leverage a new `UserDataBuilder` helper, centralizing fake data generation for improved test readability and maintainability.

---
+++ src/shared/domain/entities/entity.ts
@@ -0,0 +1,22 @@
+import { randomUUID as uuidv4 } from 'node:crypto';
+
+export abstract class Entity<Props = any > {
+    public readonly _id: string;
+    public readonly props: Props;
+
+    constructor(props: Props, id?: string) {
+        this.props = props;
+        this._id = id || uuidv4();
+    }
+
+    get id(){
+        return this._id;
+    }
+
+    toJSON(): Required<{ id: string} & Props> {
+        return {
+            id: this._id,
+            ...this.props
+        } as Required<{ id: string} & Props>;
+    }   
+}   
